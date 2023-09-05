795d2a35c6e4133b0a4f5c7fe7542a705f391bb0
Rajesh Mohan <m22aie221@iitj.ac.in>
2023-09-05 19:55:10 +0530

    assignment 3

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -16,7 +16,9 @@ import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
 from sklearn import metrics
-from utils import preprocess_data, split_data, train_model, read_digits, split_train_dev_test, predict_and_eval
+from utils import preprocess_data, split_data, train_model, read_digits, split_train_dev_test, predict_and_eval, create_combinations_dict_from_lists, tune_hparams
+import decimal
+
 gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
 C_ranges = [0.1, 1, 2, 5, 10]
 
@@ -24,42 +26,46 @@ C_ranges = [0.1, 1, 2, 5, 10]
 X, y = read_digits();
 
 #2. data splitting 
-# Split data into 70% train, 15% dev and 15% test subsets
-X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(X, y, test_sz = 0.30, dev_sz = 0.20)
+# Split data into all possbile combination of test_size_list and dev_size_list
 
-#3. Data preprocessing
-# flatten the images
-X_train = preprocess_data(X_train)
-X_dev   = preprocess_data(X_dev)
-X_test  = preprocess_data(X_test)
+test_size_list = [0.1, 0.2, 0.3]
+dev_size_list  = [0.1, 0.2, 0.3]
+
+list_of_all_test_dev_combination_dictionaries = create_combinations_dict_from_lists(test_size_list, dev_size_list)
+
+for key, value in list_of_all_test_dev_combination_dictionaries.items():
+    #split the train, dev and test
+    test_size = value[0]
+    dev_size  = value[1]
+    train_size = 1 - test_size - dev_size
+    train_size = round(train_size, 2)
+    X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(X, y, test_sz = test_size, dev_sz = dev_size)
+    #3. Data preprocessing
+    # flatten the images
+    X_train = preprocess_data(X_train)
+    X_dev   = preprocess_data(X_dev)
+    X_test  = preprocess_data(X_test)
     
-#4. Hyper paramter tuning
-#- take all combinations of gamma and C
-best_acc_so_far = -1
-best_model = None
-for cur_gamma in gamma_ranges:
-    for cur_C in C_ranges:
-        #- train the model with curr_gamma and cur_C
-        #print("Running for gamma={} c={}".format(cur_gamma, cur_C))
-        cur_model = train_model(X_train, y_train, {'gamma': cur_gamma, 'C': cur_C}, model_type="svm")
-        # - get some performance metric on DEV set
-        cur_accuracy = predict_and_eval(cur_model, X_dev, y_dev)
+    #4. Hyper paramter tuning
+    #- take all combinations of gamma and C
+    best_model = None
 
-        if cur_accuracy > best_acc_so_far:
-            print("New best accuracy: ", cur_accuracy)
-            best_acc_so_far = cur_accuracy
-            optimal_gamma = cur_gamma
-            optimal_C = cur_C
-            best_model = cur_model
-
-print("Optimal paramters gamma: ", optimal_gamma, "C: ", optimal_C)
+    list_of_all_param_combination_dictionaries = create_combinations_dict_from_lists(gamma_ranges, C_ranges)
+    best_hparams, best_model, best_accuracy    = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination_dictionaries)
 
 
-#5. Get model prediction on test set
-#6. Qualitative sanity check of the prediction
-#7. Evaluation
-test_acc = predict_and_eval(best_model, X_test, y_test)
-print("Test accuracy: ", test_acc)
+    #5. Get model prediction on test set
+    #6. Qualitative sanity check of the prediction
+    #7. Evaluation
+    
+    train_acc = predict_and_eval(best_model, X_train, y_train)
+    dev_acc   = best_accuracy
+    test_acc  = predict_and_eval(best_model, X_test, y_test)
+    
+    print("test_size = ", test_size, "dev_size = ", dev_size, "train_size = ", train_size, "train_acc = ", train_acc, "dev_acc = ", dev_acc, "test_acc = ", test_acc)
+    optimal_gamma = best_hparams[0]
+    optimal_C = best_hparams[1]
+    print("best_hparams: ", "gamma: ", optimal_gamma, "C:", optimal_C)
 
 
 

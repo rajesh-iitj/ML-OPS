cd44b8111a3766f354fcc600766e5795b9c665ab
Rajesh Mohan <m22aie221@iitj.ac.in>
2023-10-18 22:58:30 +0530

    decission tree

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -16,21 +16,36 @@ import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
 from sklearn import metrics, svm
-from utils import preprocess_data, split_data, train_model, read_digits, split_train_dev_test, predict_and_eval, create_combinations_dict_from_lists, tune_hparams
+from utils import preprocess_data, split_data, train_model, read_digits, split_train_dev_test, predict_and_eval, create_combinations_dict_from_lists, tune_hparams, get_hyperparameter_combinations
 import decimal
 from joblib import dump, load
+import pdb
 
-gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
-C_ranges = [0.1, 1, 2, 5, 10]
 
 #1. Get the digits data set with images and targets
 X, y = read_digits();
 
+#2. Hyperparamter combinations
+classifier_param_dict = {}
+#2.1 SVM
+gamma_ranges = [0.0001, 0.0005, 0.001, 0.01, 0.1, 1]
+C_ranges = [0.1, 1, 2, 5, 10]
+h_params = {}
+h_params['gamma'] = gamma_ranges
+h_params['C'] = C_ranges
+h_params_combinations = get_hyperparameter_combinations(h_params) 
+classifier_param_dict['svm'] = h_params_combinations
+#2.2 Decission trees
+max_depth_list = [5, 10, 15, 20, 50, 100]
+h_params_tree = {}
+h_params_tree['max_depth'] = max_depth_list
+h_params_combinations = get_hyperparameter_combinations(h_params_tree) 
+classifier_param_dict['tree'] = h_params_combinations
 #2. data splitting 
 # Split data into all possbile combination of test_size_list and dev_size_list
 
-test_size_list = [0.1, 0.2, 0.3]
-dev_size_list  = [0.1, 0.2, 0.3]
+test_size_list = [0.2]      #[0.1, 0.2, 0.3]
+dev_size_list  = [0.2]      #[0.1, 0.2, 0.3]
 
 list_of_all_test_dev_combination_dictionaries = create_combinations_dict_from_lists(test_size_list, dev_size_list)
 
@@ -47,32 +62,37 @@ for key, value in list_of_all_test_dev_combination_dictionaries.items():
     X_dev   = preprocess_data(X_dev)
     X_test  = preprocess_data(X_test)
     
-    #4. Hyper paramter tuning
-    #- take all combinations of gamma and C
-    best_model = None
 
-    list_of_all_param_combination_dictionaries = create_combinations_dict_from_lists(gamma_ranges, C_ranges)
-    best_hparams, best_model_path, best_accuracy    = tune_hparams(X_train, y_train, X_dev, y_dev, list_of_all_param_combination_dictionaries)
+    for model_type in classifier_param_dict:
+        #breakpoint()
+        #4. Hyper paramter tuning
+        #- take all combinations of gamma and C
+        current_hparams = classifier_param_dict[model_type]
+        best_model = None
 
-    # saving the best model
+        best_hparams, best_model_path, best_accuracy = tune_hparams(X_train, y_train, X_dev, y_dev, current_hparams, model_type)
 
-    # delete the model
+        # saving the best model
 
-    # loading of the model
-    best_model = load(best_model_path)
+        # delete the model
 
-    #5. Get model prediction on test set
-    #6. Qualitative sanity check of the prediction
-    #7. Evaluation
-    
-    train_acc = predict_and_eval(best_model, X_train, y_train)
-    dev_acc   = best_accuracy
-    test_acc  = predict_and_eval(best_model, X_test, y_test)
-    
-    print("test_size = ", test_size, "dev_size = ", dev_size, "train_size = ", train_size, "train_acc = ", train_acc, "dev_acc = ", dev_acc, "test_acc = ", test_acc)
-    optimal_gamma = best_hparams[0]
-    optimal_C = best_hparams[1]
-    print("best_hparams: ", "gamma: ", optimal_gamma, "C:", optimal_C)
+        # loading of the model
+        best_model = load(best_model_path)
+
+        #5. Get model prediction on test set
+        #6. Qualitative sanity check of the prediction
+        #7. Evaluation
+        
+        train_acc = predict_and_eval(best_model, X_train, y_train)
+        dev_acc   = best_accuracy
+        test_acc  = predict_and_eval(best_model, X_test, y_test)
+        
+        #print("{}\ttest_size={:.2f} dev_size=={:.2f} train_size={:.2f} train-acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(model_type, dev_size, train_size, train_acc, dev_acc, test_acc))
+        print("{} \t test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(model_type, test_size, dev_size, train_size, train_acc, dev_acc, test_acc))
+        #print("model: ", model_type," test_size = ", test_size, "dev_size = ", dev_size, "train_size = ", train_size, "train_acc = ", train_acc, "dev_acc = ", dev_acc, "test_acc = ", test_acc)
+        #pdb.set_trace()
+        #print("best_hparams: ", best_hparams)
+
 
 
 

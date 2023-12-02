52dcb12fbf31301b8cebb08e457aeb08e8fa97c9
Rajesh Mohan <m22aie221@iitj.ac.in>
2023-12-02 16:56:21 +0530

    docker build sript

--- docker_build.sh
+++ docker_build.sh
@@ -1,2 +1,2 @@
-docker build -t digits:v1 -f ./docker/Dockerfile .
-docker run --volume ./models:/digits/models  digits:v1
+docker build -t digits:v1 -f ./Dockerfile .
+docker run --volume ./models:/digits/models  digits:v03
